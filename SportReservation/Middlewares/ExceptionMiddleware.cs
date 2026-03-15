438e14b3a6c1d223bc7e33517d4ee57db72abbf2
MatinoTekkit <MatinoTekkit@gmail.com>
2026-03-15 14:06:46 +0100

    exception middleware

---
+++ SportReservation/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using SportReservation.Data;
+
+namespace SportReservation.Middlewares;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context, AppDbContext db)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (BadHttpRequestException exception)
+        {
+            context.Response.StatusCode = exception.StatusCode;
+            await context.Response.WriteAsJsonAsync(new { message = exception.Message });
+        }
+    }
+}
